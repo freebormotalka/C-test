d5900d2d0edd069d35d56750377cae1267fddbdf
KseniaPC <freebormotalka@gmail.com>
2023-03-12 20:12:56 +0500

    add dz third exercise

---
+++ DZ1_№6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remDiv = number % 2;
+if (remDiv == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
