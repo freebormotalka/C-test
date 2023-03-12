034422c64e7a9cc06c0946eccbdc09b90a3328ef
KseniaPC <freebormotalka@gmail.com>
2023-03-12 19:59:04 +0500

    add dz first exercise

---
+++ DZ1_№2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int max = 0;
+int min = 0;
+Console.Write("Введите первое число: ");
+int firstNumber = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+int secondNumber = Console.ReadLine();
+
+if (firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
