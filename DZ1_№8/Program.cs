e52e779327b8fbf9e7a303a6b24bfda524a59a0e
KseniaPC <freebormotalka@gmail.com>
2023-03-12 20:41:10 +0500

    edit dz

--- DZ1_№8/Program.cs
+++ DZ1_№8/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 
 int evenNumber = 2;
 
